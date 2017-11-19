c3d81ddf3586536bae2bce9d4edb236c85314a36
admin <962270789@qq.com>
2017-11-19 16:59:09 +0800

    提交订单表的sql语句

--- src/main/resources/doc/table.sql
+++ src/main/resources/doc/table.sql
@@ -12,3 +12,17 @@ CREATE TABLE `tb_feedback` (
   `create_time` datetime DEFAULT NULL COMMENT '反馈时间',
   PRIMARY KEY (`feedback_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='意见反馈表';
+
+#订单表
+CREATE TABLE `tb_order` (
+  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `order_id` bigint(50) NOT NULL COMMENT '订单号',
+  `user_id` bigint(50) NOT NULL COMMENT '购买人ID',
+  `product_name` varchar(100) NOT NULL COMMENT '商品名称',
+  `product_num` bigint(50) DEFAULT 0 COMMENT '商品数量',
+	`order_price` bigint(50) DEFAULT 0 COMMENT '订单金额',
+  `machine_num` bigint(50) DEFAULT NULL COMMENT '售卖机器编号',
+  `pay_type` bigint(5) DEFAULT NULL COMMENT '支付方式 0微信支付、1支付宝支付',
+  `create_time` datetime DEFAULT NULL COMMENT '购买时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='订单表';
