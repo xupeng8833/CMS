e352c15720df8d86a5f0f1021fb37af51ae5d059
xupeng8833 <962270789@qq.com>
2017-11-27 17:33:48 +0800

    添加用户管理的建表 sql

--- src/main/resources/doc/table.sql
+++ src/main/resources/doc/table.sql
@@ -1,72 +1,141 @@
 #建表语句
 
-#意见反馈表sql
-CREATE TABLE `tb_feedback` (
-  `feedback_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '意见反馈id',
-  `user_id` bigint(50) NOT NULL COMMENT '用户id',
-  `mobile` varchar(20) NOT NULL COMMENT '联系电话',
-  `identity` bigint(5) DEFAULT NULL COMMENT '身份:0客户、1代理商、2补货员',
-	`feedback_content` varchar(1500) DEFAULT NULL COMMENT '反馈内容',
-  `reply_type` bigint(5) DEFAULT NULL COMMENT '回复状态 0未回复、1已回复',
-  `reply_content` varchar(1500) DEFAULT NULL COMMENT '回复内容',
-  `create_time` datetime DEFAULT NULL COMMENT '反馈时间',
-  PRIMARY KEY (`feedback_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='意见反馈表';
+#终端用户的sql
+CREATE TABLE `tb_ops_customer` (
+  `ops_customer_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '终端用户id',
+  `customer_name` varchar(100) NOT NULL COMMENT '用户姓名',
+  `mobile` varchar(20) NOT NULL COMMENT '手机号',
+	`customer_password` varchar(300) DEFAULT NULL COMMENT '用户密码',
+  `purchase_amount` DECIMAL(15,2) DEFAULT 0 COMMENT '购买金额',
+	`member_points` bigint(50) DEFAULT 0 COMMENT '会员积分',
+  `customer_image` varchar(300) DEFAULT NULL COMMENT '用户头像',
+  `customer_status` bigint(5) DEFAULT 1 COMMENT '用户状态 0代表无效 1代表有效',
+  `create_time` datetime DEFAULT NULL COMMENT '注册时间',
+  PRIMARY KEY (`ops_customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='终端用户表';
 
-#订单表
-CREATE TABLE `tb_order` (
-  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `order_id` bigint(50) NOT NULL COMMENT '订单号',
-  `user_id` bigint(50) NOT NULL COMMENT '购买人ID',
-  `product_name` varchar(100) NOT NULL COMMENT '商品名称',
-  `product_num` bigint(50) DEFAULT 0 COMMENT '商品数量',
-	`order_price` bigint(50) DEFAULT 0 COMMENT '订单金额',
-  `machine_num` bigint(50) DEFAULT NULL COMMENT '售卖机器编号',
-  `pay_type` bigint(5) DEFAULT NULL COMMENT '支付方式 0微信支付、1支付宝支付',
-  `create_time` datetime DEFAULT NULL COMMENT '购买时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='订单表';
+#用户订单表sql
+CREATE TABLE `tb_ops_customer_order` (
+  `ops_customer_order_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `ops_product_name` varchar(100) NOT NULL COMMENT '商品名称',
+  `ops_customer_id` bigint(50) NOT NULL COMMENT '用户id',
+  `ops_product_amount` DECIMAL(15,2) DEFAULT NULL COMMENT '商品金额',
+  `ops_product_status` varchar(50) NOT NULL COMMENT '商品类型',
+  `ops_store_name` varchar(50) NOT NULL COMMENT '店名',
+  `ops_store_address` varchar(300) DEFAULT NULL COMMENT '商店地址',
+  `ops_desktop_machine_id` bigint(50) NOT NULL COMMENT '桌面机器编号',
+  `ops_transaction_mode` varchar(50) NOT NULL COMMENT '交易方式',
+  `ops_transaction_status` varchar(50) NOT NULL COMMENT '交易状态',
+  `ops_scene` varchar(50) NOT NULL COMMENT '场景',
+  `ops_domain` varchar(50) NOT NULL COMMENT '功能区',
+  `ops_type` varchar(50) NOT NULL COMMENT '类型',
+  `create_time` datetime DEFAULT NULL COMMENT '交易时间时间',
+  PRIMARY KEY (`ops_customer_order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户订单表';
 
+#补货员表sql
+CREATE TABLE `tb_ops_replenishment_clerk` (
+  `ops_replenishment_clerk_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '补货员id',
+  `ops_replenishment_clerk_name` varchar(100) NOT NULL COMMENT '补货员用户名',
+  `ops_replenishment_clerk_sex` bigint(5) DEFAULT 0 COMMENT '补货员性别 0代表男 1代表女',
+	`mobile` varchar(20) NOT NULL COMMENT '手机号',
+  `ops_storehouse_name` varchar(50) NOT NULL COMMENT '仓库名称',
+  `ops_city_area` varchar(100) NOT NULL COMMENT '城市区域',
+  `ops_leader_name` varchar(100) DEFAULT NULL COMMENT '上级领导',
+	`ops_duties` varchar(50) NOT NULL COMMENT '工作职能',
+	`ops_management_times` bigint(50) NOT NULL COMMENT '管理次数',
+  `create_time` datetime DEFAULT NULL COMMENT '就职时间',
+  PRIMARY KEY (`ops_replenishment_clerk_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='补货员表';
 
-#保修信息管理表
-CREATE TABLE `tb_report` (
-  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `report_num` bigint(50) NOT NULL COMMENT '报修单号',
-  `machine_num` bigint(50) NOT NULL COMMENT '报修机器编号',
-  `reporter_name` varchar(100) NOT NULL COMMENT '报修人名称',
-  `reporter_type` varchar(100) DEFAULT '0' COMMENT '报修人类型 0客户、1代理商、2补货员',
-  `repair_content` varchar(500) DEFAULT NULL,
-  `report_content` varchar(500) DEFAULT '0' COMMENT '报修备注',
-  `create_time` datetime DEFAULT NULL COMMENT '购买时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='保修信息管理';
+#修改user表的sql
+alter table sys_user add column name varchar(50) COMMENT '姓名';
+alter table sys_user add column sex BIGINT(5) DEFAULT 0 COMMENT '性别 0代表是男 1代表是女';
+alter table sys_user add column ops_leader_name varchar(50) COMMENT '所属上级';
+alter table sys_user add column ops_role_name varchar(50) COMMENT '所属部门';
+alter table sys_user add column ops_management_area varchar(100) COMMENT '管理范围';
+alter table sys_user add column ops_link_man varchar(50) COMMENT '联系人';
+alter table sys_user add column ops_income_rule varchar(50) COMMENT '收益规则';
+alter table sys_user add column ops_cooperation_policy varchar(1500) COMMENT '合作政策';
 
-#补货管理表
-CREATE TABLE `tb_replenishment` (
-  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `replenishment_num` bigint(50) NOT NULL COMMENT '补货单号',
-  `machine_num` bigint(50) NOT NULL COMMENT '补货机器编号',
-  `product_name` varchar(100) NOT NULL COMMENT '补货商品',
-  `product_num` varchar(100) DEFAULT '0' COMMENT '补充数量',
-  `apply_name` varchar(100) DEFAULT NULL COMMENT '申请人',
-  `reporter_identity` varchar(100) DEFAULT NULL COMMENT '申请人身份',
-  `apply_type` varchar(100) DEFAULT NULL COMMENT '补充状态',
-  `replenishment_name` varchar(100) NOT NULL COMMENT '补货商品',
-  `create_time` datetime DEFAULT NULL COMMENT '申请时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='保修信息管理';
+#店长，店面负责人表sql
+CREATE TABLE `tb_ops_store_manager` (
+  `ops_store_manager_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '负责人id',
+    `ops_user_id` bigint(50) NOT NULL  COMMENT '管理人id',
+  `ops_store_manager_name` varchar(100) NOT NULL COMMENT '店长姓名',
+  `sex` bigint(5) DEFAULT 0 COMMENT '店长性别 0代表男 1代表女',
+	`mobile` varchar(20) NOT NULL COMMENT '店长手机号',
+  `ops_store_manager_position` varchar(50) NOT NULL COMMENT '职位',
+  `ops_longitude_and_latitude` varchar(100) NOT NULL COMMENT '经纬度',
+  `ops_store_address` varchar(100) DEFAULT NULL COMMENT '地址',
+	`ops_store_name` varchar(50) NOT NULL COMMENT '店名称',
+	`ops_management_mode` varchar(50) NOT NULL COMMENT '经营方式',
+	`ops_scene` varchar(50) NOT NULL COMMENT '场景',
+	`ops_domain` varchar(50) NOT NULL COMMENT '功能区',
+	`ops_type` varchar(50) NOT NULL COMMENT '类型',
+	`ops_allowance` varchar(50) NOT NULL COMMENT '补贴',
+	`ops_income_rule` varchar(50) NOT NULL COMMENT '收益规则',
+  `ops_product_price` DECIMAL(15,2) DEFAULT NULL COMMENT '商品价格',
+	`ops_adsense` bigint(5) DEFAULT 0 COMMENT '是否赠送广告位 0代表不赠送 1代表赠送',
+	`ops_business_development` varchar(50) NOT NULL COMMENT 'DB名称',
+	`ops_business_mobile` varchar(20) NOT NULL COMMENT 'DB电话',
+  `ops_replenishment_clerk_id` bigint(50) NOT NULL COMMENT '补货员id',
+  `ops_replenishment_clerk_name` varchar(100) NOT NULL COMMENT '补货员用户名',
+  `ops_installation_personnel_name` varchar(100) NOT NULL COMMENT '安装人员姓名',
+  `ops_installation_personnel_mobile` varchar(20) NOT NULL COMMENT '安装人员电话',
+  `ops_linkman_name` varchar(100) NOT NULL COMMENT '联系人姓名',
+  `ops_linkman_mobile` varchar(20) NOT NULL COMMENT '联系人员电话',
+  `ops_warehouse_name` varchar(100) NOT NULL COMMENT '仓库名称',
+  `ops_point_route` varchar(100) NOT NULL COMMENT '点位路线',
+  `contract_time` datetime DEFAULT NULL COMMENT '签约时间',
+  `installation_time` datetime DEFAULT NULL COMMENT '安装时间',
+  `ops_desktop_advertising_status` bigint(5) DEFAULT 0 COMMENT '是否允许桌面广告 0允许 1不允许',
+  `ops_paper_advertising_status` bigint(5) DEFAULT 0 COMMENT '是否允许纸面广告 0允许 1不允许',
+  `ops_first_paper_feed` bigint(50) DEFAULT 0 COMMENT '首次送纸量',
+  `ops_ten_day_consumption` bigint(50) DEFAULT 0 COMMENT '10天耗质量',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`ops_store_manager_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='店长表';
 
-#提现结算申请表
-CREATE TABLE `tb_present_application` (
-  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `application_num` bigint(50) NOT NULL COMMENT '提现流水单号',
-  `apply_name_num` bigint(50) NOT NULL COMMENT '提现用户ID',
-  `account_name` varchar(100) NOT NULL COMMENT '账号名称',
-  `mobile` varchar(50) DEFAULT '0' COMMENT '手机号',
-  `operator_name` varchar(100) DEFAULT NULL COMMENT '运营商名称',
-  `cash_available` DECIMAL(15,2) DEFAULT 0 COMMENT '可提现金额',
-  `withdrawal_amount` DECIMAL(15,2) DEFAULT 0 COMMENT '提现金额',
-  `replenishment_name` bigint(10) DEFAULT 0 COMMENT '处理状态 0代表未审核 1代表已审核',
-  `create_time` datetime DEFAULT NULL COMMENT '申请时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='保修信息管理';
+#店面负责人表sql
+CREATE TABLE `tb_ops_responsible_person` (
+  `ops_responsible_person_id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '负责人id',
+  `ops_store_manager_id` bigint(50) NOT NULL  COMMENT '店长id',
+  `ops_store_manager_name` varchar(100) NOT NULL COMMENT '店长姓名',
+  `ops_responsible_person_name` varchar(100) NOT NULL COMMENT '负责人姓名',
+  `ops_group_name` varchar(100) NOT NULL COMMENT '集团名称',
+  `ops_responsible_person_mobile` varchar(100) NOT NULL COMMENT '负责人电话',
+  `sex` bigint(5) DEFAULT 0 COMMENT '店长性别 0代表男 1代表女',
+	`mobile` varchar(20) NOT NULL COMMENT '店长手机号',
+  `ops_store_manager_position` varchar(50) NOT NULL COMMENT '职位',
+  `ops_longitude_and_latitude` varchar(100) NOT NULL COMMENT '经纬度',
+  `ops_store_address` varchar(100) DEFAULT NULL COMMENT '地址',
+	`ops_store_name` varchar(50) NOT NULL COMMENT '店名称',
+	`ops_management_mode` varchar(50) NOT NULL COMMENT '经营方式',
+	`ops_scene` varchar(50) NOT NULL COMMENT '场景',
+	`ops_domain` varchar(50) NOT NULL COMMENT '功能区',
+	`ops_type` varchar(50) NOT NULL COMMENT '类型',
+	`ops_allowance` varchar(50) NOT NULL COMMENT '补贴',
+	`ops_income_rule` varchar(50) NOT NULL COMMENT '收益规则',
+  `ops_product_price` DECIMAL(15,2) DEFAULT NULL COMMENT '商品价格',
+	`ops_adsense` bigint(5) DEFAULT 0 COMMENT '是否赠送广告位 0代表不赠送 1代表赠送',
+	`ops_business_development` varchar(50) NOT NULL COMMENT 'DB名称',
+	`ops_business_mobile` varchar(20) NOT NULL COMMENT 'DB电话',
+  `ops_replenishment_clerk_id` bigint(50) NOT NULL COMMENT '补货员id',
+  `ops_replenishment_clerk_name` varchar(100) NOT NULL COMMENT '补货员用户名',
+  `ops_installation_personnel_name` varchar(100) NOT NULL COMMENT '安装人员姓名',
+  `ops_installation_personnel_mobile` varchar(20) NOT NULL COMMENT '安装人员电话',
+  `ops_linkman_name` varchar(100) NOT NULL COMMENT '联系人姓名',
+  `ops_linkman_mobile` varchar(20) NOT NULL COMMENT '联系人员电话',
+  `ops_warehouse_name` varchar(100) NOT NULL COMMENT '仓库名称',
+  `ops_point_route` varchar(100) NOT NULL COMMENT '点位路线',
+  `contract_time` datetime DEFAULT NULL COMMENT '签约时间',
+  `installation_time` datetime DEFAULT NULL COMMENT '安装时间',
+  `ops_desktop_advertising_status` bigint(5) DEFAULT 0 COMMENT '是否允许桌面广告 0允许 1不允许',
+  `ops_paper_advertising_status` bigint(5) DEFAULT 0 COMMENT '是否允许纸面广告 0允许 1不允许',
+  `ops_first_paper_feed` bigint(50) DEFAULT 0 COMMENT '首次送纸量',
+  `ops_ten_day_consumption` bigint(50) DEFAULT 0 COMMENT '10天耗质量',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`ops_responsible_person_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='负责人表';
+
